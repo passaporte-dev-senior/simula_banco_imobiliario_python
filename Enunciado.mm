5dc6c233ed6da75c717a08ea1fa3becc01f092ed
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-10-03 08:52:57 -0300

    Mapa mental inicial com base no enunciado

---
+++ Enunciado.mm
@@ -0,0 +1,131 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Enunciado" FOLDED="false" ID="ID_540221948" CREATED="1601602168875" MODIFIED="1601603475198" STYLE="oval"><hook NAME="MapStyle" background="#333333" zoom="1.464">
+    <properties edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff" show_icon_for_attributes="true" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#cccccc" BACKGROUND_COLOR="#ffffff" BACKGROUND_ALPHA="0" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#cccccc" BACKGROUND_COLOR="#3c3f41" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#dddddd" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ff3300">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffb439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#99ffff">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#bbbbbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<font SIZE="18"/>
+<node TEXT="Jogador" POSITION="right" ID="ID_1957780772" CREATED="1601602181837" MODIFIED="1601603447339" HGAP_QUANTITY="2.000000357627858 pt" VSHIFT_QUANTITY="-90.74999729543933 pt">
+<edge COLOR="#00dddd"/>
+<node TEXT="saldo" ID="ID_482787922" CREATED="1601602188133" MODIFIED="1601602855989">
+<node TEXT="300 in&#xed;cio" ID="ID_11717547" CREATED="1601722438178" MODIFIED="1601722442206"/>
+<node TEXT="+100 por rodada" ID="ID_841758145" CREATED="1601602302118" MODIFIED="1601602855988"/>
+<node TEXT="&lt;0 perde" ID="ID_1030249153" CREATED="1601602307697" MODIFIED="1601602855989"/>
+</node>
+<node TEXT="status" ID="ID_1307168566" CREATED="1601605424503" MODIFIED="1601605429569">
+<node TEXT="ativo" ID="ID_291953982" CREATED="1601605429574" MODIFIED="1601605434366"/>
+<node TEXT="inativo" ID="ID_233463298" CREATED="1601605434782" MODIFIED="1601605436563"/>
+</node>
+<node TEXT="posi&#xe7;&#xe3;o" ID="ID_974976776" CREATED="1601602190989" MODIFIED="1601722382348">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#ffff33" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_746358455" STARTINCLINATION="334;0;" ENDINCLINATION="330;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="comportamento" ID="ID_309856314" CREATED="1601602193358" MODIFIED="1601722320722">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#66ff66" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_412440787" STARTINCLINATION="479;0;" ENDINCLINATION="-5;135;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="propriedades" ID="ID_847738472" CREATED="1601602199218" MODIFIED="1601722505735">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_717268674" STARTINCLINATION="270;0;" ENDINCLINATION="270;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+<node TEXT="Propriedade" POSITION="right" ID="ID_1478809353" CREATED="1601602205437" MODIFIED="1601603470857" HGAP_QUANTITY="5.000000268220894 pt" VSHIFT_QUANTITY="64.49999807775026 pt">
+<edge COLOR="#dddd00"/>
+<node TEXT="custo de venda" ID="ID_1410950044" CREATED="1601602210458" MODIFIED="1601602855997"/>
+<node TEXT="aluguel" ID="ID_1506293234" CREATED="1601602214987" MODIFIED="1601602855998"/>
+<node TEXT="propriet&#xe1;rio" ID="ID_717268674" CREATED="1601602218280" MODIFIED="1601602855999"/>
+<node TEXT="posi&#xe7;&#xe3;o" ID="ID_746358455" CREATED="1601603630937" MODIFIED="1601722415267">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#ff9933" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1933060438" STARTINCLINATION="295;146;" ENDINCLINATION="253;155;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+<node TEXT="Comportamento" POSITION="left" ID="ID_412440787" CREATED="1601602422370" MODIFIED="1601603449803" HGAP_QUANTITY="2.000000357627858 pt" VSHIFT_QUANTITY="-62.999998122453746 pt">
+<edge COLOR="#dddd7c"/>
+<node TEXT="impulsivo" ID="ID_1794080468" CREATED="1601602427288" MODIFIED="1601602856010">
+<node TEXT="compra sempre" ID="ID_1653847129" CREATED="1601602431864" MODIFIED="1601602856010"/>
+</node>
+<node TEXT="exigente" ID="ID_1542057674" CREATED="1601602434904" MODIFIED="1601602856011">
+<node TEXT="compra se aluguel &gt;50" ID="ID_929112253" CREATED="1601602448300" MODIFIED="1601602856011"/>
+</node>
+<node TEXT="cauteloso" ID="ID_1649249765" CREATED="1601602437804" MODIFIED="1601602856012">
+<node TEXT="compra se saldo &gt;=80 ap&#xf3;s compra" ID="ID_1539124497" CREATED="1601602465406" MODIFIED="1601602856012"/>
+</node>
+<node TEXT="aleat&#xf3;rio" ID="ID_154939480" CREATED="1601602442506" MODIFIED="1601602856012">
+<node TEXT="50 % probabilidade" ID="ID_1661551388" CREATED="1601602487890" MODIFIED="1601602856012"/>
+</node>
+</node>
+<node TEXT="Dado" POSITION="left" ID="ID_1906511088" CREATED="1601602224454" MODIFIED="1601603475197" HGAP_QUANTITY="41.749999172985575 pt" VSHIFT_QUANTITY="8.249999754130847 pt">
+<edge COLOR="#dd0000"/>
+<node TEXT="aleat&#xf3;rio" ID="ID_903491283" CREATED="1601602228835" MODIFIED="1601603606516">
+<node TEXT="6" OBJECT="java.lang.Long|6" ID="ID_1888996290" CREATED="1601603606517" MODIFIED="1601603607646"/>
+</node>
+</node>
+<node TEXT="Jogo" POSITION="left" ID="ID_1412825861" CREATED="1601602232675" MODIFIED="1601603463878" HGAP_QUANTITY="20.749999798834327 pt" VSHIFT_QUANTITY="37.49999888241295 pt">
+<edge COLOR="#00dd00"/>
+<node TEXT="n&#xfa;mero de rodadas" ID="ID_1439487931" CREATED="1601602242134" MODIFIED="1601602856004"/>
+<node TEXT="quantidade de propriedades" ID="ID_731781476" CREATED="1601602248453" MODIFIED="1601602856005"/>
+</node>
+<node TEXT="Tabuleiro" POSITION="left" ID="ID_1807439054" CREATED="1601602353976" MODIFIED="1601603465831" HGAP_QUANTITY="30.499999508261695 pt" VSHIFT_QUANTITY="32.249999038875124 pt">
+<edge COLOR="#7cdddd"/>
+<node TEXT="quantidade de posi&#xe7;&#xf5;es" ID="ID_999098341" CREATED="1601602357238" MODIFIED="1601602856007"/>
+<node TEXT="posi&#xe7;&#xe3;o" ID="ID_1933060438" CREATED="1601602380431" MODIFIED="1601602856008">
+<node TEXT="propriedade" ID="ID_821717039" CREATED="1601602386306" MODIFIED="1601602856008"/>
+<node TEXT="jogador" ID="ID_1622829466" CREATED="1601602391347" MODIFIED="1601602856008"/>
+</node>
+</node>
+</node>
+</map>
